72daa1bf051821f7e86a37b6f459392a352fbfb1
Matthias Tavasszy <mtavasszy@student.tudelft.nl>
2015-09-08 11:00:43 +0200

    add comments to Sound class

--- src/main/Sound.java
+++ src/main/Sound.java
@@ -7,14 +7,35 @@ import java.io.IOException;
 import sun.audio.AudioPlayer;
 import sun.audio.AudioStream;
 
+/**
+ * Sound class: this class represents a playable audio file.
+ * 
+ * @author Matthias Tavasszy
+ *
+ */
+@SuppressWarnings("restriction")
 public class Sound {
 
+	/**
+	 * filename.
+	 */
 	private String fileName;
 
-	public Sound(String fName) {
+	/**
+	 * Sound: constructor.
+	 * 
+	 * @param fName
+	 */
+	public Sound(final String fName) {
 		fileName = fName;
 	}
 
+	/**
+	 * play: play the sound.
+	 * 
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
 	@SuppressWarnings("restriction")
 	public void play() throws FileNotFoundException, IOException {
 			AudioPlayer.player.start(new AudioStream(new FileInputStream(
