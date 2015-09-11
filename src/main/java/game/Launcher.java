3e531d479a0f9c78a2e3c21115f00d31d2c32991
Bart Ziengs <a.h.ziengs@student.tudelft.nl>
2015-09-11 20:19:05 +0200

    text rendering (not finished, dont include diz plzz)

---
+++ src/main/java/game/Launcher.java
@@ -0,0 +1,181 @@
+package game;
+import static org.lwjgl.glfw.Callbacks.errorCallbackPrint;
+import static org.lwjgl.glfw.GLFW.GLFW_RESIZABLE;
+import static org.lwjgl.glfw.GLFW.GLFW_VISIBLE;
+import static org.lwjgl.glfw.GLFW.glfwCreateWindow;
+import static org.lwjgl.glfw.GLFW.glfwDefaultWindowHints;
+import static org.lwjgl.glfw.GLFW.glfwDestroyWindow;
+import static org.lwjgl.glfw.GLFW.glfwGetPrimaryMonitor;
+import static org.lwjgl.glfw.GLFW.glfwGetTime;
+import static org.lwjgl.glfw.GLFW.glfwGetVideoMode;
+import static org.lwjgl.glfw.GLFW.glfwInit;
+import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
+import static org.lwjgl.glfw.GLFW.glfwPollEvents;
+import static org.lwjgl.glfw.GLFW.glfwSetErrorCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetKeyCallback;
+import static org.lwjgl.glfw.GLFW.glfwShowWindow;
+import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
+import static org.lwjgl.glfw.GLFW.glfwSwapInterval;
+import static org.lwjgl.glfw.GLFW.glfwTerminate;
+import static org.lwjgl.glfw.GLFW.glfwWindowHint;
+import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_FALSE;
+import static org.lwjgl.opengl.GL11.GL_TRUE;
+import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.opengl.GL11.glClearColor;
+import static org.lwjgl.opengl.GL11.glLoadIdentity;
+import static org.lwjgl.opengl.GL11.glMatrixMode;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+import org.lwjgl.Sys;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWKeyCallback;
+import org.lwjgl.glfw.GLFWvidmode;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GLContext;
+
+import utillities.Keyboard;
+import font.FontTT;
+/**
+ * 
+ * JAAAAAASPSPSEEEEEERRRR
+ *
+ */
+public class Launcher {
+
+	// We need to strongly reference callback instances.
+	private GLFWErrorCallback errorCallback;
+	private GLFWKeyCallback keyCallback;
+	private double lastFrame;
+	private int WIDTH;
+	private int HEIGHT;
+	// The window handle
+	private long window;
+	public static FontTT myFont;
+	
+	/**dont include this in the hand in commit!**/
+
+	public void run() throws FontFormatException, IOException {
+		System.out.println("Hello LWJGL " + Sys.getVersion() + "!");
+
+		try {
+			init();
+			loop();
+
+			// Release window and window callbacks
+			glfwDestroyWindow(window);
+			keyCallback.release();
+		} finally {
+			// Terminate GLFW and release the GLFWerrorfun
+			glfwTerminate();
+			errorCallback.release();
+		}
+	}
+
+	private void init() throws FontFormatException, IOException {
+		// Setup an error callback. The default implementation
+		// will print the error message in System.err.
+		glfwSetErrorCallback(errorCallback = errorCallbackPrint(System.err));
+
+		// Initialize GLFW. Most GLFW functions will not work before doing this.
+		if (glfwInit() != GL11.GL_TRUE) {
+			throw new IllegalStateException("Unable to initialize GLFW");
+		}
+
+		// Configure our window
+		glfwDefaultWindowHints(); // optional, the current window hints are
+									// already the default
+		glfwWindowHint(GLFW_VISIBLE, GL_FALSE); // the window will stay hidden
+												// after creation
+		glfwWindowHint(GLFW_RESIZABLE, GL_TRUE); // the window will be resizable
+		// Get the resolution of the primary monitor
+		ByteBuffer vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+		WIDTH = 1000;//GLFWvidmode.width(vidmode);
+		HEIGHT = 600;GLFWvidmode.height(vidmode);
+
+		// Create the window
+		 window = glfwCreateWindow(WIDTH, HEIGHT, "Hello World!", NULL, NULL);
+		// Fullscreen
+		//window = glfwCreateWindow(WIDTH, HEIGHT, "Hello World!",
+		//		glfwGetPrimaryMonitor(), NULL);
+		if (window == NULL)
+			throw new RuntimeException("Failed to create the GLFW window");
+
+		glfwSetKeyCallback(window, keyCallback = new Keyboard());
+		// Make the OpenGL context current
+		glfwMakeContextCurrent(window);
+		// Enable v-sync
+		glfwSwapInterval(1);
+		// Make the window visible
+		glfwShowWindow(window);
+	}
+
+	private void loop() throws FontFormatException, IOException {
+		// This line is critical for LWJGL's interoperation with GLFW's
+		// OpenGL context, or any context that is managed externally.
+		// LWJGL detects the context that is current in the current thread,
+		// creates the ContextCapabilities instance and makes the OpenGL
+		// bindings available for use.
+		GLContext.createFromCurrent();
+
+		// Set the clear color
+		glClearColor(0.0f, 0.3f, 0.3f, 0.0f);
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
+		GL11.glDisable(GL11.GL_TEXTURE_2D);
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glEnable(GL11.GL_ALPHA);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+		GL11.glMatrixMode(GL11.GL_PROJECTION);
+		GL11.glLoadIdentity();
+		GL11.glOrtho(-WIDTH / 2, WIDTH / 2, 0, HEIGHT, -1, 1);
+		glMatrixMode(GL11.GL_MODELVIEW);
+
+		int fontresolution = 16;		
+		myFont = new FontTT(Font.createFont(Font.TRUETYPE_FONT, new File("levels/cambria.ttc")), fontresolution, 2 );
+		Game game = new Game();
+		// Run the rendering loop until the user has attempted to close
+		// the window or has pressed the ESCAPE key.
+		while (glfwWindowShouldClose(window) == GL_FALSE) {
+			glClear(GL_COLOR_BUFFER_BIT);
+			glLoadIdentity();
+
+			game.update(getDelta());
+			game.render();
+
+			glfwSwapBuffers(window); // swap the color buffers
+
+			// Poll for window events. The key callback above will only be
+			// invoked during this call.
+			glfwPollEvents();
+		}
+	}
+
+	public double getDelta() {
+		double time = glfwGetTime();
+		double delta = time - lastFrame;
+		lastFrame = time;
+
+		return delta;
+	}
+
+	public static void main(String[] args) {
+		try {
+			new Launcher().run();
+		} catch (FontFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
