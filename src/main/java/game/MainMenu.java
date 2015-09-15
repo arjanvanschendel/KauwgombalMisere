d90d04a2e83d4165b5e7a7bcefe4914ad0c8415f
Arjan <a.j.vanschendel@student.tudelft.nl>
2015-09-15 10:57:05 +0200

    Working text yay

--- src/main/java/game/MainMenu.java
+++ src/main/java/game/MainMenu.java
@@ -1,20 +1,29 @@
 package game;
 
+import java.awt.Font;
 import java.util.ArrayList;
-
-import org.newdawn.slick.Color;
 
 import objects.GameObject;
 
+import org.lwjgl.opengl.GL11;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.opengl.TextureImpl;
+import org.newdawn.slick.TrueTypeFont;
+
+
 public class MainMenu {
+	
+
+	
+	Font awtFont = new Font("Comic Sans MS", Font.PLAIN, 48);
+	TrueTypeFont font = new TrueTypeFont(awtFont, true);
 	
 	private static ArrayList<GameObject> objects = new ArrayList<GameObject>();
 	/**
 	 * update: update the level-object's state.
-	 * 
 	 * @param deltaTime
 	 */
-	public final void update(double deltaTime) {
+	public final void update(final double deltaTime) {
 		//for (GameObject update : objects) {
 		//	update.update(deltaTime);
 		//}
@@ -24,7 +33,10 @@ public class MainMenu {
 	 * render: render the level-object's graphics.
 	 */
 	public final void render() {
-		Launcher.font.drawString(-50, -100, "SAMPLE TEXT" , Color.white);
+
+		GL11.glScalef(1, -1, 1);
+		TextureImpl.bindNone();
+		font.drawString(0, -500, "Kauwgombal Misere" , Color.cyan);
 		
 		//for (GameObject render : objects) {
 		//	render.render();
