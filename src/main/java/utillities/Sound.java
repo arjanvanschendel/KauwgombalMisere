0e8428857b19112929bec9ecae09ac20201c9fe4
Matthias Tavasszy <mtavasszy@student.tudelft.nl>
2015-09-15 09:50:46 +0200

    add loop method

--- src/main/java/utillities/Sound.java
+++ src/main/java/utillities/Sound.java
@@ -4,8 +4,12 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import sun.audio.AudioData;
+import sun.audio.AudioDataStream;
 import sun.audio.AudioPlayer;
 import sun.audio.AudioStream;
+import sun.audio.ContinuousAudioDataStream;
+
 
 /**
  * Sound class: this class represents a playable audio file.
@@ -20,14 +24,23 @@ public class Sound {
 	 * filename.
 	 */
 	private String fileName;
+	private AudioData audiodata;
+	private AudioDataStream audiostream;
+	private ContinuousAudioDataStream continuousaudiostream;
+
 
 	/**
 	 * Sound: constructor.
 	 * 
-	 * @param fName
+	 * @param filename
+	 * @throws IOException
 	 */
-	public Sound(final String fName) {
-		fileName = fName;
+	public Sound(final String filename) throws IOException {
+		FileInputStream fis = new FileInputStream(filename);
+		AudioStream audioStream = new AudioStream(fis);
+		audiodata = audioStream.getData();
+		audiostream = null;
+		continuousaudiostream = null;
 	}
 
 	/**
@@ -38,8 +51,20 @@ public class Sound {
 	 */
 	@SuppressWarnings("restriction")
 	public void play() throws FileNotFoundException, IOException {
-			AudioPlayer.player.start(new AudioStream(new FileInputStream(
-					fileName)));
+		audiostream = new AudioDataStream(audiodata);
+		AudioPlayer.player.start(audiostream);
 	}
+
+	/**
+	 * loop: loop the sound.
+	 * 
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
+	public void loop() throws FileNotFoundException, IOException {
+		continuousaudiostream = new ContinuousAudioDataStream(audiodata);
+		AudioPlayer.player.start(continuousaudiostream);
+	}
+
 
 }
