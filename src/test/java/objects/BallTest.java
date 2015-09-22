b20f44f3f35a2b0ec2ba07b3e68b45a413c0cc5d
Matthias Tavasszy <mtavasszy@student.tudelft.nl>
2015-09-22 10:05:19 +0200

    introduce BallTest.java <testing

--- src/test/java/objects/BallTest.java
+++ src/test/java/objects/BallTest.java
@@ -1,14 +1,38 @@
 package objects;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import org.junit.Before;
+import org.junit.Test;
 
 /**
- * @author Bart
+ * @author Matthias
  *
  */
 
-import org.junit.Test;
-
 public class BallTest {
+
+	Ball g;
+	Ball g1;
+	Ball g2;
+	Ball b;
+	Ball r;
+
+	@Before
+	public void setup() {
+		g = new Ball(0, 0, 50 / 4);
+		g1 = new Ball(0, 0, 50 / 4);
+		g2 = new Ball(1, 0, 50 / 4);
+		b = new Ball(0, 0, 50 / 2);
+		r = new Ball(0, 0, 50);
+	}
+
+	@Test
+	public void equalsTest() {
+		assertEquals(g, g1);
+		assertNotEquals(g, g2);
+		assertNotEquals(b, r);
+	}
 
 }
