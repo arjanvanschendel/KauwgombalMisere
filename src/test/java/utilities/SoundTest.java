fa30a20499db93e3fd7dcc00dbb2dc16f1661444
Matthias Tavasszy <matt.tavasszy@gmail.com>
2015-10-04 17:18:34 +0200

    expand the SoundTest test class <testing

--- src/test/java/utilities/SoundTest.java
+++ src/test/java/utilities/SoundTest.java
@@ -1,5 +1,6 @@
 package utilities;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -9,28 +10,44 @@ import org.junit.Test;
 
 import utillities.Sound;
 
+/**
+ * Tests the Sound class.
+ * @author Matthias
+ *
+ */
 public class SoundTest {
 
+	/**
+	 * Sound.
+	 */
 	private Sound snd;
+	/**
+	 * Sound with extra volume.
+	 */
+	private Sound snd2;
 
+	/**
+	 * setup: set up all objects before executing each test case.
+	 */
 	@Before
-	public void setup() {
+	public final void setup() {
 		snd = new Sound("res/Screaming_ducks.wav");
+		snd2 = new Sound("res/Screaming_ducks.wav", 5);
 	}
 
 	/**
-	 * make sure the music doesn't keep playing.
+	 * teardown: make sure the music doesn't keep playing.
 	 */
 	@After
-	public void teardown() {
+	public final void teardown() {
 		snd.stop();
 	}
 
 	/**
-	 * test the start function.
+	 * test the play function.
 	 */
 	@Test
-	public void startTest() {
+	public final void playTest() {
 		snd.play();
 
 		assertTrue(snd.isPlaying());
@@ -40,11 +57,87 @@ public class SoundTest {
 	 * test the stop function.
 	 */
 	@Test
-	public void stopTest() {
+	public final void stopPlayTest() {
 		snd.play();
 		assertTrue(snd.isPlaying());
 		snd.stop();
 		assertFalse(snd.isPlaying());
-
 	}
+	
+	/**
+	 * test the loop function.
+	 */
+	@Test
+	public final void loopTest() {
+		snd.loop(5);
+
+		assertTrue(snd.isPlaying());
+	}
+
+	/**
+	 * test the stop function.
+	 */
+	@Test
+	public final void stopLoopTest() {
+		snd.loop(5);
+		assertTrue(snd.isPlaying());
+		snd.stop();
+		assertFalse(snd.isPlaying());
+	}
+
+	/**
+	 * test the play function.
+	 */
+	@Test
+	public final void playTest2() {
+		snd2.play();
+
+		assertTrue(snd2.isPlaying());
+	}
+
+	/**
+	 * test the stop function.
+	 */
+	@Test
+	public final void stopPlayTest2() {
+		snd2.play();
+		assertTrue(snd2.isPlaying());
+		snd2.stop();
+		assertFalse(snd2.isPlaying());
+	}
+	
+	/**
+	 * test the loop function.
+	 */
+	@Test
+	public final void loopTest2() {
+		snd2.loop(5);
+
+		assertTrue(snd2.isPlaying());
+	}
+
+	/**
+	 * test the stop function.
+	 */
+	@Test
+	public final void stopLoopTest2() {
+		snd2.loop(5);
+		assertTrue(snd2.isPlaying());
+		snd2.stop();
+		assertFalse(snd2.isPlaying());
+	}
+	
+	/**
+	 * volumeCapTest: tests if the volume cap works.
+	 */
+	@Test
+	public final void volumeCapTest() {
+		Sound snd3 = new Sound("res/Screaming_ducks.wav", -10);
+		assertTrue(snd3.getVolume() == -6f);
+		snd3.setVolume(10f);
+		assertTrue(snd3.getVolume() == 10f);
+		snd3.setVolume(-10f);
+		assertTrue(snd3.getVolume() == -6f);
+	}
+
 }
