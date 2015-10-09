b60848acc4cd550a456001c39956c99290dabb5b
Jasper van Esveld <J.M.vanEsveld@student.tudelft.nl>
2015-10-09 16:13:32 +0200

    Added the new option menu state to game. Also reorganized the sounds and added a nice music file. Added missing GameSettings for the option menu. <feature,organization

--- src/main/java/Menu/MainMenu.java
+++ src/main/java/Menu/MainMenu.java
@@ -20,79 +20,86 @@ import org.newdawn.slick.opengl.TextureImpl;
 
 /**
  * Draws and updates the main menu.
+ * 
  * @author Arjan
  *
  */
 public class MainMenu {
-		
+
 	private Font awtFont = new Font("Courier New", Font.BOLD, 48);
 	private TrueTypeFont font = new TrueTypeFont(awtFont, true);
-	private ArrayList<Button> buttons  = new ArrayList<Button>();
+	private ArrayList<Button> buttons = new ArrayList<Button>();
 	private Button playBtn;
 	private Button exitBtn;
 	private Button optBtn;
 	private Texture Background;
 	private int WIDTH;
 	private int HEIGHT;
-	
+
 	/**
 	 * A class to draw and maintain the main menu.
 	 */
 	public MainMenu() {
-		playBtn = new Button((float)-75, (float)Launcher.getCAMHEIGHT()/3+40,
-				(float)150, (float)25, java.awt.Color.white, "Play");
+		playBtn = new Button((float) -75,
+				(float) Launcher.getCAMHEIGHT() / 3 + 40, (float) 150,
+				(float) 25, java.awt.Color.white, "Play");
 		buttons.add(playBtn);
-		optBtn = new Button((float)-75, (float)Launcher.getCAMHEIGHT()/3, 
-				(float)150, (float)25, java.awt.Color.white, "Options");
+		optBtn = new Button((float) -75, (float) Launcher.getCAMHEIGHT() / 3,
+				(float) 150, (float) 25, java.awt.Color.white, "Options");
 		buttons.add(optBtn);
-		exitBtn = new Button((float)-75, (float)Launcher.getCAMHEIGHT()/3-40, 
-				(float)150, (float)25, java.awt.Color.white, "Exit");
+		exitBtn = new Button((float) -75,
+				(float) Launcher.getCAMHEIGHT() / 3 - 40, (float) 150,
+				(float) 25, java.awt.Color.white, "Exit");
 		buttons.add(exitBtn);
-		Background = new Texture("res/KMmain.png",
-				GL11.GL_NEAREST, GL11.GL_CLAMP);
-		
+		Background = new Texture("res/KMmain.png", GL11.GL_NEAREST,
+				GL11.GL_CLAMP);
+
 		WIDTH = Launcher.getCAMWIDTH();
 		HEIGHT = Launcher.getCAMHEIGHT();
 		System.out.println("WxH = " + WIDTH + "x" + HEIGHT);
-		
+
 	}
-	
+
 	/**
 	 * Finds first positive power of two above the given input.
-	 * @param number any integer
+	 * 
+	 * @param number
+	 *            any integer
 	 * @return a power of two
 	 */
 	public static int findNextTwo(final int number) {
 		int result = 1;
-		while (result < number){
+		while (result < number) {
 			result *= 2;
 		}
 		return result;
 	}
-	
+
 	/**
 	 * update: update the menu-object's state.
-	 * @param deltaTime The speed of the game
+	 * 
+	 * @param deltaTime
+	 *            The speed of the game
 	 */
 	public final void update(final double deltaTime) {
-		if(!Game.sounds.get(5).isPlaying()){
-			Game.sounds.get(5).play();
+		if (!Game.sounds.get(5).isPlaying()) {
+			Game.sounds.get(5).loop(-1);
 		}
-		
+
 		for (Button btn : buttons) {
 			btn.update(deltaTime);
 		}
-		
 		if (playBtn.isClicked()) {
 			Game.loadLevel(1);
 			Game.setState(0);
 		}
 		if (optBtn.isClicked()) {
 			System.out.println("Options button clicked");
+			Game.setState(3);
 		}
 		if (exitBtn.isClicked()) {
 			System.out.println("Exiting system via exit button");
-			Game.setState(3);
+			Game.setState(4);
 		}
 	}
 
@@ -101,31 +108,32 @@ public class MainMenu {
 	 */
 	public final void render() {
 		Background.bind(); // or GL11.glBind(texture.getTextureID());
-         
-        GL11.glBegin(GL11.GL_QUADS);
+		GL11.glBegin(GL11.GL_QUADS);
 		glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
-		
-            GL11.glTexCoord2f(0, 0);
-            GL11.glVertex2f(-(float)WIDTH / 2, HEIGHT);
-            
-            GL11.glTexCoord2f(1, 0);
-            GL11.glVertex2f((float)WIDTH / 2, HEIGHT);
-            
-            GL11.glTexCoord2f(1, 1);
-            GL11.glVertex2f((float)WIDTH / 2, 0);
-            
-            GL11.glTexCoord2f(0, 1);
-            GL11.glVertex2f(-(float)WIDTH / 2, 0);
-        GL11.glEnd();
-        Texture.disable();
+
+		GL11.glTexCoord2f(0, 0);
+		GL11.glVertex2f(-(float) WIDTH / 2, HEIGHT);
+
+		GL11.glTexCoord2f(1, 0);
+		GL11.glVertex2f((float) WIDTH / 2, HEIGHT);
+
+		GL11.glTexCoord2f(1, 1);
+		GL11.glVertex2f((float) WIDTH / 2, 0);
+
+		GL11.glTexCoord2f(0, 1);
+		GL11.glVertex2f(-(float) WIDTH / 2, 0);
+		GL11.glEnd();
+		Texture.disable();
 		for (Button btn : buttons) {
 			btn.render();
 		}
-		
+
 		GL11.glScalef(1, -1, 1);
 		TextureImpl.bindNone();
 		font.drawString(0, -100, "sample text", Color.cyan);
 		GL11.glScalef(1, -1, 1);
 		GL11.glDisable(GL11.GL_TEXTURE_2D);
+
+		
 	}
 }
--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -15,6 +15,7 @@ import org.newdawn.slick.Color;
 import org.newdawn.slick.opengl.TextureImpl;
 
 import Menu.MainMenu;
+import Menu.OptionMenu;
 import utillities.Keyboard;
 import utillities.Logger;
 import utillities.Mouse;
@@ -27,279 +28,293 @@ import utillities.Texture;
  * Class Game: a Game object represents a game, holding all the levels.
  */
 public class Game {
-    private static int state;
-    private static Level currentLvl;
-    private static int lvl;
-    private int maxLvl;
-    private MainMenu mm;
-    private static int lives;
-    public static final ArrayList<Sound> sounds = new ArrayList<Sound>();
-    public static final ArrayList<Texture> textures = new ArrayList<Texture>();
-    private static ArrayList<GameObject> popUpObjects = new ArrayList<GameObject>();
+	private static int state;
+	private static Level currentLvl;
+	private static int lvl;
+	private int maxLvl;
+	private MainMenu mm;
+	private OptionMenu om;
+	private static int lives;
+	public static final ArrayList<Sound> sounds = new ArrayList<Sound>();
+	public static final ArrayList<Texture> textures = new ArrayList<Texture>();
+	private static ArrayList<GameObject> popUpObjects = new ArrayList<GameObject>();
 
-	
-    /**
-     * Game: constructor.
-     */
-    public Game() {
-	setLives(3);
-	Logger.add("game started");
-	loadTextures();
-	loadSounds();
-	setLvl(1);
-	maxLvl = countLevels();
-	loadLevel(lvl);
-	mm = new MainMenu();
-	setState(2);
-    }
-    
-    private void loadSounds() {
+	/**
+	 * Game: constructor.
+	 */
+	public Game() {
+		setLives(3);
+		Logger.add("game started");
+		loadTextures();
+		loadSounds();
+		setLvl(1);
+		maxLvl = countLevels();
+		loadLevel(lvl);
+		mm = new MainMenu();
+		om = new OptionMenu();
+		setState(2);
+	}
 
-		sounds.add(new Sound("sounds/arrowHitBall.wav"));
-		sounds.add(new Sound("sounds/arrowHitCeiling.wav"));
-		sounds.add(new Sound("sounds/arrowShoot.wav"));
-		sounds.add(new Sound("sounds/ballBounce.wav"));
-		sounds.add(new Sound("sounds/playerHit.wav"));
+	private void loadSounds() {
+
+		sounds.add(new Sound("sounds/sfx/arrowHitBall.wav", GameSettings
+				.getSFXVolume()));
+		sounds.add(new Sound("sounds/sfx/arrowHitCeiling.wav", GameSettings
+				.getSFXVolume()));
+		sounds.add(new Sound("sounds/sfx/arrowShoot.wav", GameSettings
+				.getSFXVolume()));
+		sounds.add(new Sound("sounds/sfx/ballBounce.wav", GameSettings
+				.getSFXVolume()));
+		sounds.add(new Sound("sounds/sfx/playerHit.wav", GameSettings
+				.getSFXVolume()));
 		// Main menu sound
-		sounds.add(new Sound("sounds/arrowHitCeiling.wav"));
+		sounds.add(new Sound("sounds/music/Solar_Sailer.wav", GameSettings
+				.getMusicVolume()));
 	}
 
-    /**
-     * loadTextures: load the textures onto memory.
-     */
-    private void loadTextures() {
-	textures.add(0, new Texture("res/IdleSprite.png", GL11.GL_NEAREST,
-		GL11.GL_REPEAT));
-	textures.add(1, new Texture("res/Run.png", GL11.GL_NEAREST,
-		GL11.GL_REPEAT));
-	textures.add(2, new Texture("res/arrow.png", GL11.GL_NEAREST,
-		GL11.GL_CLAMP));
-    }
-
-    /**
-     * loadLevel: load a level.
-     * 
-     * @param location
-     */
-    public static void loadLevel(int number) {
-	setLvl(number);
-	Logger.add("loading in new level");
-	currentLvl = new Level("levels/level" + number + ".lvl");
-    }
-
-    /**
-     * nextLevel: moves the player to the next level.
-     */
-    public void nextLevel() {
-	if (lvl < maxLvl) {
-	    Logger.add("next level");
-	    setLvl(getLvl() + 1);
-	    loadLevel(lvl);
-	} else {
-	    Logger.add("game won");
-	    setState(3);
+	/**
+	 * loadTextures: load the textures onto memory.
+	 */
+	private void loadTextures() {
+		textures.add(0, new Texture("res/IdleSprite.png", GL11.GL_NEAREST,
+				GL11.GL_REPEAT));
+		textures.add(1, new Texture("res/Run.png", GL11.GL_NEAREST,
+				GL11.GL_REPEAT));
+		textures.add(2, new Texture("res/arrow.png", GL11.GL_NEAREST,
+				GL11.GL_CLAMP));
 	}
 
-    }
+	/**
+	 * loadLevel: load a level.
+	 * 
+	 * @param location
+	 */
+	public static void loadLevel(int number) {
+		setLvl(number);
+		Logger.add("loading in new level");
+		currentLvl = new Level("levels/level" + number + ".lvl");
+	}
 
-    /**
-     * countLevels: count the amount of level files.
-     * 
-     * @return integer result
-     */
-    public int countLevels() {
-	int result = 0;
-	File f = new File("levels/level" + (result + 1) + ".lvl");
-	while (f.exists() && !f.isDirectory()) {
-	    result++;
-	    f = new File("levels/level" + (result + 1) + ".lvl");
+	/**
+	 * nextLevel: moves the player to the next level.
+	 */
+	public void nextLevel() {
+		if (lvl < maxLvl) {
+			Logger.add("next level");
+			setLvl(getLvl() + 1);
+			loadLevel(lvl);
+		} else {
+			Logger.add("game won");
+			setState(3);
 		}
-	return result;
-    	
-    }
-
-    /**
-     * update: Update the state of the game.
-     * 
-     * @param deltaTime
-     */
-    public void update(double deltaTime) {
-	if (Keyboard.isKeyReleased(GLFW_KEY_ESCAPE)) {
-	    if (state == 0) {
-		setState(1);
-	    } else if (state == 1) {
-		setState(0);
-	    }
-	}
-	if (Keyboard.isKeyReleased(GLFW_KEY_SPACE)) {
-	    if (state == 2) {
-		setState(0);
-	    }
-	}
-	if (Level.levelComplete() && state == 0) {
-	    nextLevel();
-	}
-
-	switch (state) {
-	case (0):
-	    // Playing
-	    currentLvl.update(deltaTime);
-		updateThePopUps(deltaTime);
-	    break;
-	case (1):
-	    // Paused
-	    break;
-
-	case (2):
-	    // Main Menu
-	    mm.update(deltaTime);
-	    break;
-	case (3):
-	    System.out.println("new state");
-	    break;
-	default:
-	    System.out.println("INVALID STATE: " + state
-		    + ". (Game.update method)");
-	    setState(2);
-	    // System.exit(1);
 
 	}
-	Mouse.resetReleased();
-	Keyboard.resetReleased();
-    }
-    
-    public static void addPopUp(ScorePopUp popUp){
-    if(popUpObjects.size() > 3){
-    	for(int i = 0; i < popUpObjects.size()-1; ++i){
-    		popUpObjects.remove(0);
-    	}
-    }
-    	popUpObjects.add(popUp);
-    }
-    
-    public void updateThePopUps(double deltaTime) {
-	for (GameObject popup : popUpObjects) {
-	   	popup.update(deltaTime);
+
+	/**
+	 * countLevels: count the amount of level files.
+	 * 
+	 * @return integer result
+	 */
+	public int countLevels() {
+		int result = 0;
+		File f = new File("levels/level" + (result + 1) + ".lvl");
+		while (f.exists() && !f.isDirectory()) {
+			result++;
+			f = new File("levels/level" + (result + 1) + ".lvl");
+		}
+		return result;
+
 	}
-    }
-    
-    public void renderThePopUps(){
-    	for (GameObject render : popUpObjects) {
-    	    render.render();
-    	}
-    }
 
-    /**
-     * render: Render the graphics of the game.
-     */
-    public void render() {
-	switch (state) {
-	case (0):
-	    // game
-	    currentLvl.render();
-		TextureImpl.bindNone();
-		renderThePopUps();
-	    GL11.glScalef(1, -1, 1);
-	  //  TextureImpl.bindNone();
-	    String levelString = "Level " + lvl + ": " + currentLvl.getName();
-	    String scoreString = "Score: "+Level.getScore();
-	    String livesString = "Lives : " + lives;
-	    
-	    //Draw scoreString
-	    Launcher.getFont().drawString( -400, -540, scoreString, Color.blue);
+	/**
+	 * update: Update the state of the game.
+	 * 
+	 * @param deltaTime
+	 */
+	public void update(double deltaTime) {
+		if (Keyboard.isKeyReleased(GLFW_KEY_ESCAPE)) {
+			if (state == 0) {
+				setState(1);
+			} else if (state == 1) {
+				setState(0);
+			}
+		}
+		if (Keyboard.isKeyReleased(GLFW_KEY_SPACE)) {
+			if (state == 2) {
+				setState(0);
+			}
+		}
+		if (Level.levelComplete() && state == 0) {
+			nextLevel();
+		}
 
-	    //Draw levelString
-	    Launcher.getFont().drawString(
-		    -(float) Launcher.getFont().getWidth(levelString) / 2,
-		    -100, levelString, Color.black);
-	    
-	    //Draw livesString
-	    Launcher.getFont().drawString(
-			    -(float) Launcher.getFont().getWidth(livesString) / 2,
-			    -70, livesString, Color.black);
-	    
-	    GL11.glScalef(1, -1, 1);
-	    GL11.glDisable(GL11.GL_TEXTURE_2D);
-	    break;
-	case (1):
-	    // paused
-	    currentLvl.render();
-	    GL11.glScalef(1, -1, 1);
-	    TextureImpl.bindNone();
-	    Launcher.getFont().drawString(-50, -300, "PAUSED", Color.yellow);
-	    GL11.glScalef(1, -1, 1);
-	    GL11.glDisable(GL11.GL_TEXTURE_2D);
-	    break;
-	case (2):
-	    mm.render();
-	    break;
-	default:
-	    System.out.println("INVALID STATE: " + state
-		    + ". (Game.render method)");
-	    // System.exit(1);
-	    setState(2);
+		switch (state) {
+		case (0):
+			// Playing
+			currentLvl.update(deltaTime);
+			updateThePopUps(deltaTime);
+			break;
+		case (1):
+			// Paused
+			break;
+
+		case (2):
+			// Main Menu
+			mm.update(deltaTime);
+			break;
+		case (3):
+			// Options Menu
+			om.update(deltaTime);
+			break;
+		default:
+			System.out.println("INVALID STATE: " + state
+					+ ". (Game.update method)");
+			setState(2);
+			// System.exit(1);
+
+		}
+		Mouse.resetReleased();
+		Keyboard.resetReleased();
 	}
-    }
 
-    public static void ballhit(float x, float y, char size) {
-	String s;
-	if (size == 'b') {
-	    s = "20";
-	} else {
-	    s = "10";
+	public static void addPopUp(ScorePopUp popUp) {
+		if (popUpObjects.size() > 3) {
+			for (int i = 0; i < popUpObjects.size() - 1; ++i) {
+				popUpObjects.remove(0);
+			}
+		}
+		popUpObjects.add(popUp);
 	}
-    }
 
-    /**
-     * getState.
-     * @return int state
-     */
-    public final int getState() {
-	return state;
-    }
+	public void updateThePopUps(double deltaTime) {
+		for (GameObject popup : popUpObjects) {
+			popup.update(deltaTime);
+		}
+	}
 
-    /**
-     * setState.
-     * 
-     * @param state
-     */
-    public static void setState(int newState) {
-	Game.state = newState;
-    }
+	public void renderThePopUps() {
+		for (GameObject render : popUpObjects) {
+			render.render();
+		}
+	}
 
-    /**
-     * @return the lvl
-     */
-    public static final int getLvl() {
-	return lvl;
-    }
+	/**
+	 * render: Render the graphics of the game.
+	 */
+	public void render() {
+		switch (state) {
+		case (0):
+			// game
+			currentLvl.render();
+			TextureImpl.bindNone();
+			renderThePopUps();
+			GL11.glScalef(1, -1, 1);
+			// TextureImpl.bindNone();
+			String levelString = "Level " + lvl + ": " + currentLvl.getName();
+			String scoreString = "Score: " + Level.getScore();
+			String livesString = "Lives : " + lives;
 
-    /**
-     * @param lvl
-     *  the lvl to set
-     */
-    public static final void setLvl(int lvl) {
-	Game.lvl = lvl;
-    }
+			// Draw scoreString
+			Launcher.getFont().drawString(-400, -540, scoreString, Color.blue);
 
-    /**
-     * @return the lives
-     */
-    public final static int getLives() {
-	return lives;
-    }
+			// Draw levelString
+			Launcher.getFont().drawString(
+					-(float) Launcher.getFont().getWidth(levelString) / 2,
+					-100, levelString, Color.black);
 
-    /**
-     * @param lives the lives to set
-     */
-    public final static void setLives(int lives) {
-	Game.lives = lives;
-    }
-    
-    /**
-     * @param lives the lives to set
-     */
-    public final static void decreaseLives() {
-	Game.lives--;
-    }
+			// Draw livesString
+			Launcher.getFont().drawString(
+					-(float) Launcher.getFont().getWidth(livesString) / 2, -70,
+					livesString, Color.black);
+
+			GL11.glScalef(1, -1, 1);
+			GL11.glDisable(GL11.GL_TEXTURE_2D);
+			break;
+		case (1):
+			// paused
+			currentLvl.render();
+			GL11.glScalef(1, -1, 1);
+			TextureImpl.bindNone();
+			Launcher.getFont().drawString(-50, -300, "PAUSED", Color.yellow);
+			GL11.glScalef(1, -1, 1);
+			GL11.glDisable(GL11.GL_TEXTURE_2D);
+			break;
+		case (2):
+			mm.render();
+			break;
+		case (3):
+			om.render();
+			break;
+		default:
+			System.out.println("INVALID STATE: " + state
+					+ ". (Game.render method)");
+			// System.exit(1);
+			setState(2);
+		}
+	}
+
+	public static void ballhit(float x, float y, char size) {
+		String s;
+		if (size == 'b') {
+			s = "20";
+		} else {
+			s = "10";
+		}
+	}
+
+	/**
+	 * getState.
+	 * 
+	 * @return int state
+	 */
+	public final int getState() {
+		return state;
+	}
+
+	/**
+	 * setState.
+	 * 
+	 * @param state
+	 */
+	public static void setState(int newState) {
+		Game.state = newState;
+	}
+
+	/**
+	 * @return the lvl
+	 */
+	public static final int getLvl() {
+		return lvl;
+	}
+
+	/**
+	 * @param lvl
+	 *            the lvl to set
+	 */
+	public static final void setLvl(int lvl) {
+		Game.lvl = lvl;
+	}
+
+	/**
+	 * @return the lives
+	 */
+	public final static int getLives() {
+		return lives;
+	}
+
+	/**
+	 * @param lives
+	 *            the lives to set
+	 */
+	public final static void setLives(int lives) {
+		Game.lives = lives;
+	}
+
+	/**
+	 * @param lives
+	 *            the lives to set
+	 */
+	public final static void decreaseLives() {
+		Game.lives--;
+	}
 }
